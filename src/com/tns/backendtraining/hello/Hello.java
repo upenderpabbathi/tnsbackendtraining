1ee0e32b536b8d3e840f63413dafcfac2d2f2ab1
upend <upend@192.168.1.60>
2025-08-04 23:01:12 +0530

    Second commit

--- src/com/tns/backendtraining/hello/Hello.java
+++ src/com/tns/backendtraining/hello/Hello.java
@@ -1,10 +1,13 @@
 package com.tns.backendtraining.hello;
 
 public class Hello {
-
-	public static void main(String[] args) {
-		System.out.print("Hello!! Welcome to TNS");
-
+	void print() {
+		System.out.print("Welcome to TNS Backend Training");
 	}
 
-}
+	public static void main(String[] args) {
+		Hello obj = new Hello();
+		obj.print();
+	}
+		
+	}
